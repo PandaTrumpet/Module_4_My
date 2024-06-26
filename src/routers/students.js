32c522febba69efb564ca765ff07836cf49ae562
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-26 17:08:16 +0300

    update

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 // import { getAllStudents, getStudentById } from '../service/students.js';
-import { createStudentSchema } from '../validation/student.js';
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from '../validation/student.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -12,15 +15,28 @@ import {
   upsertStudentController,
   patchStudentController,
 } from '../controllers/students.js';
+import { isValidId } from '../middlewares/isValidId.js';
 const router = Router();
 router.get('/students', ctrlWrapper(getStudentsController));
-router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+router.get(
+  '/students/:studentId',
+  isValidId(),
+  ctrlWrapper(getStudentByIdController),
+);
 router.post(
   '/students',
   validateBody(createStudentSchema),
   ctrlWrapper(createStudentController),
 );
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
-router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.put(
+  '/students/:studentId',
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+router.patch(
+  '/students/:studentId',
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
+);
 export default router;
