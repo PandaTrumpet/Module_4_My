93620eeccbf17a4aecc28e28321a90fdc19171bc
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-26 16:58:46 +0300

    updtae

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 // import { getAllStudents, getStudentById } from '../service/students.js';
+import { createStudentSchema } from '../validation/student.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
 import {
   getStudentByIdController,
   getStudentsController,
@@ -12,7 +15,11 @@ import {
 const router = Router();
 router.get('/students', ctrlWrapper(getStudentsController));
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
-router.post('/students', ctrlWrapper(createStudentController));
+router.post(
+  '/students',
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
 router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
 router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
---
+++ src/validation/student.js
@@ -0,0 +1,27 @@
+import Joi from 'joi';
+export const createStudentSchema = Joi.object({
+  name: Joi.string().max(30).min(3).required(),
+  email: Joi.string().email().required(),
+  age: Joi.number().integer().min(6).max(16).required(),
+  gender: Joi.string().valid('male', 'female', 'other').required(),
+  avgMark: Joi.number().min(2).max(12).required(),
+  onDuty: Joi.boolean(),
+});
+//Пример проверки
+// const dataToValidate = {
+//   name: 'John Doe',
+//   email: 'john.doe@example.com',
+//   age: 12,
+//   gender: 'male',
+//   avgMark: 10.2,
+// };
+
+// const validationResult = createStudentSchema.validate(dataToValidate);
+// if (validationResult.error) {
+//   console.error(validationResult.error.message);
+// } else {
+//   console.log('Data is valid!');
+// }
+// const validationResult = createStudentSchema.validate(userData, {
+//   abortEarly: false,
+// });
